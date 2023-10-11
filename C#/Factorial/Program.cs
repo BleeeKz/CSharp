364bfa22159ccbb4ab4f4e477592c0ceccec37e9
BleeeKz <102173717+BleeeKz@users.noreply.github.com>
2023-10-11 14:50:35 +0200

    commit c1

---
+++ C#/Factorial/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+
+        public static long Factorial(long x)
+        {
+            long res = -1;
+            if (x==0)
+                res = 1;
+            else
+            {
+                if (x>0)
+                {
+                    res = x;
+                    while (x>1)
+                    {
+                        res = res * (x - 1);
+                        x--;
+                    }
+                }
+                else
+                {
+                    throw new Exception("error");
+                }
+            }
+
+            return res;
+
+        }
+
+        public static void quieroElDiez(int x)
+        {
+
+            int cont = 1;
+            int num = 0;
+
+            if (cont==0)
+            {
+                throw new Exception("error");
+            }
+
+            while (cont <= x)
+            {
+                num+=2;
+                cont++;
+                Console.WriteLine(Factorial(num));
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            quieroElDiez(10);
+
+        }
+    }
+}
